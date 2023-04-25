795c976554464525c2a70002a6e1103351c1ad95
Shuyibe48 <gthrough48@gmail.com>
2023-04-25 21:37:28 +0600

    dragon server add on github

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000
+
+const categories = require('./data/categories.json')
+const cors = require('cors')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('The Dragon Server is running...')
+})
+
+app.get('/categories', (req, res) => {
+    res.send(categories)
+})
+
+app.listen(port, () => {
+    console.log(`the app is running on port ${port}`)
+})
