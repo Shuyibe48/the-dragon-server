698daf082bbce559e26778e0bae200e526c9af11
Shuyibe48 <gthrough48@gmail.com>
2023-04-26 14:35:00 +0600

    news data share

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 const cors = require('cors')
 
 app.use(cors())
@@ -13,6 +14,24 @@ app.get('/', (req, res) => {
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id 
+    res.send(news.find(n => n._id === id))
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    if(id === 0){
+        res.send(news)
+    }else{
+        res.send(news.filter(n => parseInt(n.category_id) === id))
+    }
 })
 
 app.listen(port, () => {
